906a091ce99e24ee435c8d5fb3349a43e488b1c5
Walid <walid_douri@hotmail.com>
2022-01-03 17:01:58 -0500

    Planning & Exceution

---
+++ main.rb
@@ -0,0 +1,59 @@
+=begin
+
+Task 1
+Extract Nouns for Classes
+Game
+Players
+  1 & 2/Current
+Turns
+Questions
+Answer
+Lives
+
+
+Task 2
+
+game
+to keep track of all questions and current players turns 
+to have controls in place to insure that two different players are playing 
+
+Player
+To keep record of W/L ratios 
+Players basic info - name etc.
+list of players
+
+
+class Game
+  attr_accessor :name
+  attr_reader :Player_1 
+  attr_reader :Player_2
+
+  def initialize(name)
+    @name = name
+    @Player_1 = Player.new("Player 1")
+    @Player_2 = Player.new("Player 2")
+  end
+
+  def 
+
+end
+
+class Players
+  player_1
+  player_2
+
+
+
+
+Task 3
+
+
+
+
+
+
+
+
+
+
+=end
